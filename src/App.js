90bc232a95194e56c95ff665fe2062c65f455d77
bidhanroyjoy <bidhan19994@gmail.com>
2021-10-11 00:49:08 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import "./App.css";
 import Header from "./components/Header/Header";
-import { useDispatch, useSelector } from "react-redux";
-import { GetApiDataAction } from "./Redux/Action";
-import { useEffect } from "react";
 // import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home";
 
